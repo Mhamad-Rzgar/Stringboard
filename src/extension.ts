44da6b59ed04953619f717c15ce7f9060c930048
Mohammed Rzgar <mhamadplay4@gmail.com>
2026-05-17 06:37:21 +0300

    update publisher name and icon of the extentions.

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@ import * as vscode from 'vscode';
 import StringboardPanel from './panel/stringboardPanel';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Stringboard extension activated.');
 
 	const openCommand = vscode.commands.registerCommand('stringboard.open', async () => {
 		await StringboardPanel.createOrShow(context.extensionUri);
